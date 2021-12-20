41b4c3d9922aa4132e416d6a7b717767a7727c55
thomas1y <thomasyohannes15@gmail.com>
2021-12-20 11:00:37 -0800

    make it better

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,36 +1,72 @@
 import React from 'react'
-import {LinkContainer} from 'react-router-bootstrap'
-import { Navbar,Nav,Container } from 'react-bootstrap';
-//import { FaShoppingCart,FaRegUser } from "react-icons/fa";
+import { Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import SearchBox from './SearchBox'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
-    return <header>
-        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-  <Container>
-    <LinkContainer to='/'>
-    <Navbar.Brand>E-commerce</Navbar.Brand>
-    </LinkContainer>
+  const dispatch = useDispatch()
 
-    
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ms-auto">
-        <LinkContainer to="/cart">
-          <Nav.Link  ><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/login">
-             
-        <Nav.Link ><i className='fas fa-user'></i> Sign In</Nav.Link>
-        </LinkContainer>
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
-   
-        
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Ecommerce</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Route render={({ history }) => <SearchBox history={history} />} />
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/productlist'>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
+  )
 }
 
-export default Header;
-
+export default Header
