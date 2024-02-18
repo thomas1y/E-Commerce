e895b6c7833422c0252873daf70ed2389bfbd889
thomas1y <thomasyohannes15@gmail.com>
2024-02-19 01:49:28 +0300

    first commit

--- client/src/screens/OrderScreen.js
+++ client/src/screens/OrderScreen.js
@@ -74,6 +74,7 @@ const OrderScreen = ({ match, history }) => {
         setSdkReady(true)
       }
     }
+    
   }, [dispatch, orderId, successPay, successDeliver, order])
 
   const successPaymentHandler = (paymentResult) => {
